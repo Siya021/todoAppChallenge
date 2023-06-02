d1f618c9cb1c4348f97d8ae1d2343ae535e0ea66
Siya021 <siyamthandamazokwana@gmail.com>
2023-06-02 16:24:09 +0200

    jsFirst run

--- script/code.js
+++ script/code.js
@@ -1,8 +1,36 @@
+const todoList=document.querySelector("#screen");
+const addList = document.querySelector("addTask");
+const sortList = document.querySelector("sortTask");
 let array =[
     {
         id: 1,
-        name: '',
-        createdDate:'',
-        completed:'',
-}
+        name: 'jbkhj',
+        createdDate: Date.now(),
+        completed: false,
+    },
+    {
+        id: 2,
+        name: 'uyvjygv',
+        createdDate: Date.now(),
+        completed: false,
+    },
 ]
+function newTask(){
+    event.preventDefault();
+    if(screen.value === ''){
+        alert("Task is missing. Add Task!!!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = screen.value;
+        itemsList.appendChild(li)
+    }
+}
+
+array.forEach((item)=>{
+    document.querySelector('#itemsList').innerHTML += 
+    `
+    <li>${item.name}</li>
+    `
+
+})
